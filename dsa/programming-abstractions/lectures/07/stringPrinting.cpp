1a5b982824564a4704ba1707308afa4815851cae
Sami932 <jongwoni0516@gmail.com>
2026-05-07 05:56:10 -0400

    An alternative approach to this problem would be to print the last character of the string in each recursive call

--- dsa/programming-abstractions/lectures/07/stringPrinting.cpp
+++ dsa/programming-abstractions/lectures/07/stringPrinting.cpp
@@ -34,12 +34,26 @@ void printStringReverse(string s)
 
 }
 
+// An alternative approach to this problem would be to print the last character of the string in each recursive call
+void printStringReverse2(string s)
+{
+    if (s.length() == 0)
+    {
+        cout << endl;
+        return;
+    }
+
+    cout << s[s.length() - 1];
+    printStringReverse2(s.substr(0, s.length() - 1));
+}
+
 int main()
 {
     // Calling twice to ensure proper placement of line break in output.
     printString("hello");
     printString("hello");
 
+    // the output actually places the newline character before each reversed string, not after. 
     printStringReverse("hello");
     printStringReverse("hello");
 
