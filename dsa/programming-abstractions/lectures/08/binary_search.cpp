f4ac7da1a09d4f34597b0ef9e00e36da198d5d07
Sami932 <jongwoni0516@gmail.com>
2026-05-09 14:15:12 -0400

    binary search

---
+++ dsa/programming-abstractions/lectures/08/binary_search.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include "console.h"
+#include "random.h"
+#include "simpio.h"
+#include "vector.h"
+using namespace std;
+
+// Binary search from positions lo through hi (inclusive) for our key. Return first
+// index where found, or -1 if not found. Passing vector by reference for efficiency.
+int binarySearch(Vector<int>& v, int key, int lo, int hi)
+{
+    // Base case: If our indices have crossed over, the key must not be present.
+    if (lo > hi)
+    {
+        return -1;
+    }
+
+    int mid = lo + (hi - lo) / 2;
+
+    if (key < v[mid])
+    {
+        return binarySearch(v, key, lo, mid - 1);
+    }
+    else if (key > v[mid])
+    {
+        return binarySearch(v, key, mid + 1, hi);
+    }
+    else
+    {
+        return mid;
+    }
+}
+
+// Wrapper function for binary search. This is the one our client would call.
+int binarySearch(Vector<int>& v, int key)
+{
+    return binarySearch(v, key, 0, v.size() - 1);
+}
+
+Vector<int> createSortedRandoVector(int n)
+{
+    Vector<int> v;
+
+    for (int i = 0; i < n; i++)
+    {
+        v.add(randomInteger(0, 50));
+    }
+
+    v.sort();
+    return v;
+}
+
+int main()
+{
+    Vector<int>v = createSortedRandomVector(10);
+    cout << v << endl;
+
+    while (true)
+    {
+        cout << endl << "What integer shall we search for? (-1 to quit) ";
+        int key = getInteger();
+
+        if (key == -1)
+        {
+            break;
+        }
+
+        cout << "-> result: " << binarySearch(v, key) << endl;
+    }
+
+    cout << "Bye!" << endl;
+    return 0;
+}
