460fabeea158c569518f461c3b90c500bb1a9973
Sami932 <jongwoni0516@gmail.com>
2026-05-13 23:56:51 -0400

    class terminologies

--- dsa/programming-abstractions/lectures/12/main.cpp
+++ dsa/programming-abstractions/lectures/12/main.cpp
@@ -34,3 +34,18 @@ int main()
 
     return 0;
 }
+
+/*
+We refer to the variables within a class as its MEMBER VARIABLES.
+We refer to the functions within a class as its MEMBER FUNCTIONS.
+We often say that member variables define the STATE of an object, while member functions define the BEHAVIORS an object can perform.
+A function whose name matches the name of the class where it resides is a CONSTRUCTOR function.
+
+Every Quokka we create gets its own copies of the variables above. When that happens, we refer to those variables as INSTANCE VARIABLES. 
+In OOP, we sometimes have variables that are shared across all instances of a class. You might hear me use the terms "member variables" 
+and "instance variables" interchangeably to refer to the sorts of variables we're defining above.
+
+Note that it's considered best practice not to issue a using namespace std; statement in our quokka.h file. If we did that, 
+then that namespace would apply to every source file with an #include "quokka.h" directive. We instead prefer to qualify 
+anything from the std namespace in our header file using the tedious std:: syntax.
+*/
