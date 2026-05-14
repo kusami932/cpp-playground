4b8b7a0e603027edc45da9504c8a14a326d5e746
Sami932 <jongwoni0516@gmail.com>
2026-05-14 13:32:27 -0400

    looping through an array using a pointer

--- dsa/programming-abstractions/lectures/13/arraypoint.cpp
+++ dsa/programming-abstractions/lectures/13/arraypoint.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main()
 {
-    int array[5];
+    int array[5] = {10, 15, 20, 25, 30};
 
-    // Prints addresses of all cells in the array.
+    int *p = array;
+
     for (int i = 0; i < 5; i++)
     {
-        cout << "&(array[" << i << "]): " << &(array[i]) << endl;
+        cout << p[i] << endl;
     }
 
-    // Prints base address of array. Notice this is the same as &(array[0]).
-    cout << endl << "Base address of array: " << array << endl;
-
     return 0;
+    
 }
 
 /*
-Array looks a lot like a pointer. It contains, in fact, the address of an integer -- the first integer in our array. 
-It stands to reason that if we create a pointer of type int *, we should be able to use that to loop through our array, as well. 
+while an array might look a lot like a pointer, the key difference is that a pointer can be assigned a new value, 
+causing it to point elsewhere. An array variable name, on the other hand, is bound to the array it represents 
+and cannot be made to point elsewhere.
 */
