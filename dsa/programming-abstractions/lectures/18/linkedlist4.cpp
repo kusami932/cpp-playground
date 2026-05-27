55f3bb4c7b2bf3d70182465071e31cfb8198f00e
Sami932 <jongwoni0516@gmail.com>
2026-05-27 02:55:44 -0400

    tail pointers

--- dsa/programming-abstractions/lectures/18/linkedlist4.cpp
+++ dsa/programming-abstractions/lectures/18/linkedlist4.cpp
@@ -58,3 +58,40 @@ int offWithItsHead(Node *&head)
     delete oldHead;
     return retrieval;
 }
+
+/*
+Head insertion is an O(1) operation. Regardless of how long our linked list is, adding a new element 
+to the beginning of our list is simply a matter of creating a new node, setting its next pointer to 
+the old head of our linked list, and setting the head to point to our newly created node. 
+All of that can be done in O(1) time.
+
+The tail insertion function we saw last time had a linear runtime, but today we explored an idea for 
+speeding that up: maintaining a pointer not just to the head of a linked list, but also to the tail! 
+If we do that, we can achieve O(1) insertion at the tail of our list: we just set the tail's next pointer 
+to a new node, then move the tail pointer forward. This is a ✨dramatic✨ improvement in runtime, and it 
+only costs us two things: (1) the memory associated with maintaining a tail pointer (typically just 8 bytes 
+in C++) and (2) the complexity and effort associated with writing code that correctly keeps track of the tail pointer.
+
+Maintaining a tail pointer actually even makes our head insertion function more complex. That's because 
+if we pass an empty list to our head insertion function, the new node we create will not only be the 
+first node in the list, but also the last. If we have just one node in our list, it's both the head 
+and the tail -- and so we need our head insertion function to be able to update both our head and tail pointers.
+*/
+
+void tailInsert(Node *&head, Node *&tail, int data)
+{
+    // If tail is nullptr, that means the list is currently empty, and the new node
+    // we're creating will be the first node in our list. That means that it is both
+    // the head and tail of the list, so we set both of those pointers here.
+    if (tail == nullptr)
+    {
+        head = tail = new Node(data);
+        return;
+    }
+
+    // Otherwise, if we already have a tail node, simply tack a new node on the end
+    // of the list right after it, and then move the tail pointer forward to point
+    // to that new node. Tada!
+    tail->next = new Node(data);
+    tail = tail->next;
+}
