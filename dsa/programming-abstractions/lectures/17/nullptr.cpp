ad74d55f58ceef6cfb2d3bc56f35dcecdc7ee9e6
Sami932 <jongwoni0516@gmail.com>
2026-05-27 01:39:06 -0400

    coding defensively - check whether a pointer is null or not before dereferencing it

---
+++ dsa/programming-abstractions/lectures/17/nullptr.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "console.h"
+using namespace std;
+
+int main()
+{
+    int *ptr = nullptr;
+
+    // Suppose we have many lines of code here, and the logic is complex and twisty,
+    // and we aren't quite sure at first glance whether ptr has ever been updated to
+    // point to an actual integer. If not, we don't want to dereference it, because
+    // that could lead to a segmentation fault and crash our program.
+    //
+    // No worries! We can just check whether its still nullptr, like so:
+
+    if (ptr != nullptr)
+    {
+        cout << "Aha! It's safe to dereference ptr." << endl;
+        *ptr = 50;
+    }
+    else
+    {
+        cout << "OoOOOOoh! We didn't dereference the pointer! Hooray!" << endl;
+    }
+
+    return 0;
+}
